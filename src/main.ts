e61136b4c7325aec14375b26dbaeb64796f87e98
YanHeng Zhou <1801084586@qq.com>
2025-04-28 17:34:38 +0800

    feat:register校验

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); //全局启用 ValidationPipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // 自动将请求参数转换为 DTO 类的实例
+      // whitelist: true, // 只允许 DTO 类中定义的属性
+      // forbidNonWhitelisted: true, // 禁止非 DTO 类中定义的属性
+    }),
+  ); //全局启用 ValidationPipe
   await app.listen(3000);
 }
 bootstrap();
